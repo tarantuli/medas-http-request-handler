cfe860446d2fc148bb7782afe3d2c9c61226f3a0
Patrick Morpey <patrick@morphp.nl>
2022-04-22 10:19:14 +0200

    Added JsonLdHandler

---
+++ src/Exceptions/NoJsonLdResponseException.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\HttpRequestHandler\Exceptions;
+
+use Medas\Core\Exceptions\BaseException;
+use Medas\HttpRequestHandler\ResponseTypes\Response;
+
+class NoJsonLdResponseException extends BaseException
+{
+    public function __construct(Response $response)
+    {
+        parent::__construct($response::class);
+    }
+
+    public function pattern(): string
+    {
+        return 'JSON LD response requested, but %s does not implement JsonLdResponse';
+    }
+}
---
+++ src/ResponseHandlers/JsonLdHandler.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\HttpRequestHandler\ResponseHandlers;
+
+use Medas\HttpRequestHandler\Exceptions\NoJsonLdResponseException;
+use Medas\HttpRequestHandler\Request\Request;
+use Medas\HttpRequestHandler\ResponseTypes\JsonLdResponse;
+use Medas\HttpRequestHandler\ResponseTypes\Response;
+
+class JsonLdHandler implements ResponseHandler
+{
+    public function priority(): int
+    {
+        return -5;
+    }
+
+    public function handleResponse(Request $request, Response $response): bool
+    {
+        if ($request->uri->extension === 'jsonld') {
+            if (!$response instanceof JsonLdResponse) {
+                throw new NoJsonLdResponseException($response);
+            }
+
+            // Else, fall through to the echo command
+        }
+        elseif (!$request->serverData->acceptsMimeType('application/ld+json') || !$response instanceof JsonLdResponse) {
+            return false;
+        }
+
+        echo json_encode($response->getJsonLdResponse());
+        return true;
+    }
+
+    public function handleException(Request $request, \Exception|\TypeError|\Error $exception): bool
+    {
+        if (!$request->serverData->acceptsMimeType('application/ld+json')) {
+            return false;
+        }
+
+        // todo: craft a real jsonld error response
+        echo json_encode([
+            'message' => $exception->getMessage(),
+            'code' => $exception->getCode(),
+            'fileName' => $exception->getFile(),
+            'lineNumber' => $exception->getLine(),
+
+        ]);
+
+        return true;
+    }
+}
