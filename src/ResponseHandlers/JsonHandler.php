8132efbfd741ebda4f2cc2f954ae6b055f45a429
Patrick Morpey <patrick@morphp.nl>
2023-02-02 09:58:24 +0100

    Force message to be UTF-8

--- src/ResponseHandlers/JsonHandler.php
+++ src/ResponseHandlers/JsonHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Medas\HttpRequestHandler\ResponseHandlers;
 
+use Medas\Core\Str;
 use Medas\HttpRequestHandler\Exceptions\NoJsonResponseException;
 use Medas\HttpRequestHandler\Request\Request;
 use Medas\HttpRequestHandler\ResponseHandlerManager;
@@ -56,7 +57,7 @@ class JsonHandler implements ResponseHandler
         $manager->setHeader('Access-Control-Allow-Origin', '*');
 
         echo json_encode([
-            'message' => $exception->getMessage(),
+            'message' => Str::forceUtf8($exception->getMessage()),
             'code' => $exception->getCode(),
             'fileName' => $exception->getFile(),
             'lineNumber' => $exception->getLine(),
