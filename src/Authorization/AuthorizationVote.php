181a485ca0b52efb015e7c0c040ba81b16b88cc2
Patrick Morpey <patrick@morphp.nl>
2025-06-30 09:27:14 +0200

    Updated dependencies

--- src/Authorization/AuthorizationVote.php
+++ src/Authorization/AuthorizationVote.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Medas\HttpRequestHandler\Authorization;
 
-use Medas\Core\Interfaces\HttpRequestHandler;
+use Medas\Core\{Events\BasicVote, Interfaces\HttpRequestHandler};
 use Medas\HttpRequestHandler\Request\Request;
 
 class AuthorizationVote extends BasicVote
