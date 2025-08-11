309f01afe69539055e3a4e5b9c44df3a95ffbd6a
Patrick Morpey <patrick@morphp.nl>
2025-08-11 15:37:01 +0200

    Use dispatch(()

--- src/Request/AuthenticationFinder.php
+++ src/Request/AuthenticationFinder.php
@@ -4,23 +4,17 @@ declare(strict_types=1);
 
 namespace Medas\HttpRequestHandler\Request;
 
-use Medas\Core\{Attributes\Service, Interfaces\EventDispatcher};
+use Medas\Core\Attributes\Service;
 use Medas\HttpRequestHandler\Authentication\AuthenticationVote;
 
 #[Service]
 readonly class AuthenticationFinder
 {
-    public function __construct(
-        public EventDispatcher $eventDispatcher,
-    )
-    {
-    }
-
     public function find(Request $request): void
     {
         $authenticationVote = new AuthenticationVote($request);
 
-        $this->eventDispatcher->dispatch($authenticationVote);
+        dispatch($authenticationVote);
 
         $request->authentication->user = $authenticationVote->user;
     }
