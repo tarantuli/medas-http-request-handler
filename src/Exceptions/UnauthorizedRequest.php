da2d50d0e22adfaf33a7b9320839d4695d0183a1
Patrick Morpey <patrick@morphp.nl>
2026-02-02 17:19:01 +0100

    Refactored `UnauthorizedRequest` to extend `BadRequest` instead of `BaseException`.

--- src/Exceptions/UnauthorizedRequest.php
+++ src/Exceptions/UnauthorizedRequest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Medas\HttpRequestHandler\Exceptions;
 
-use Medas\Core\Exceptions\BaseException;
-
-abstract class UnauthorizedRequest extends BaseException
+abstract class UnauthorizedRequest extends BadRequest
 {
 }
