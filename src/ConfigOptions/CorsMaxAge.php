57edeb66dac31f1441747536d9b08ce10a2c44bb
Patrick Morpey <patrick@morphp.nl>
2026-03-24 11:25:55 +0100

    Added `CorsMaxAge` configuration option and updated CORS header logic to include `Access-Control-Max-Age`. Updated dependencies.

---
+++ src/ConfigOptions/CorsMaxAge.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\HttpRequestHandler\ConfigOptions;
+
+use Medas\Core\{Attributes\Service, Interfaces\ConfigGroup, Interfaces\ConfigOption};
+
+#[Service]
+readonly class CorsMaxAge implements ConfigOption
+{
+    public function __construct(
+        private HttpRequestHandlerGroup $group,
+    )
+    {
+    }
+
+    public function group(): ConfigGroup
+    {
+        return $this->group;
+    }
+
+    public function name(): string
+    {
+        return 'cors-max-age';
+    }
+
+    public function description(): string
+    {
+        return 'The maximum age (in seconds) that the browser should cache CORS preflight requests';
+    }
+
+    public function hasDefault(): bool
+    {
+        return true;
+    }
+
+    public function default(): int
+    {
+        return 3600;
+    }
+}
--- src/ResponseHandlers/CorsHeaderWriter.php
+++ src/ResponseHandlers/CorsHeaderWriter.php
@@ -7,6 +7,7 @@ namespace Medas\HttpRequestHandler\ResponseHandlers;
 use Medas\Core\Attributes\{ConfigValue, Service};
 use Medas\HttpRequestHandler\{
     ConfigOptions\CorsAllowedOrigins,
+    ConfigOptions\CorsMaxAge,
     ResponseDispatcher\ExceptionJob,
     ResponseDispatcher\Job
 };
@@ -16,31 +17,42 @@ readonly class CorsHeaderWriter
 {
     public function __construct(
         #[ConfigValue(CorsAllowedOrigins::class)]
-        private string $corsAllowedOrigins,
+        private string $allowedOrigins,
+
+        #[ConfigValue(CorsMaxAge::class)]
+        private int    $maxAge,
     )
     {
     }
 
     public function handle(Job|ExceptionJob $job): void
     {
-        if ($this->corsAllowedOrigins === '') {
+        if ($this->allowedOrigins === '') {
             return;
         }
 
         $requestOrigin = $job->request->serverData['HTTP_ORIGIN'] ?? '';
 
-        if ($this->corsAllowedOrigins === '*') {
-            $job->headers['Access-Control-Allow-Origin'] = $requestOrigin;
-            $job->headers['Access-Control-Allow-Credentials'] = 'true';
+        if ($this->allowedOrigins === '*') {
+            $this->addHeaders($requestOrigin, $job);
 
             return;
         }
 
-        $allowedOrigins = array_map('trim', explode(',', $this->corsAllowedOrigins));
+        $allowedOrigins = array_map('trim', explode(',', $this->allowedOrigins));
 
         if (in_array($requestOrigin, $allowedOrigins, true)) {
-            $job->headers['Access-Control-Allow-Origin'] = $requestOrigin;
-            $job->headers['Access-Control-Allow-Credentials'] = 'true';
+            $this->addHeaders($requestOrigin, $job);
+        }
+    }
+
+    private function addHeaders(mixed $requestOrigin, Job|ExceptionJob $job): void
+    {
+        $job->headers['Access-Control-Allow-Origin'] = $requestOrigin;
+        $job->headers['Access-Control-Allow-Credentials'] = 'true';
+
+        if ($this->maxAge > 0) {
+            $job->headers['Access-Control-Max-Age'] = (string) $this->maxAge;
         }
     }
 }
